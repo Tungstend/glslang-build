20d255e7bd6be497eda465a2ea6f691e76d917fc
Tungstend <74163103+Tungstend@users.noreply.github.com>
2025-03-25 00:57:24 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+apt update
+apt install git python3 python-is-python3 zip unzip -y
+
+git clone https://github.com/KhronosGroup/glslang.git
+
+chmod -R 777 glslang
+
+wget -nc -nv -O  https://dl.google.com/android/repository/android-ndk-r27b-linux.zip
+unzip -q android-ndk-r27b-linux.zip
+export ANDROID_NDK_HOME=$PWD/android-ndk-r27b
+
+cd glslang
+./update_glslang_sources.py
+
+
+
+cmake -B $PWD/build -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$PWD/install/$ABI" -DANDROID_ABI=$ABI -DCMAKE_BUILD_TYPE=Release -DANDROID_STL=c++_static -DANDROID_PLATFORM=android-21 -DCMAKE_SYSTEM_NAME=Android -DANDROID_TOOLCHAIN=clang -DANDROID_ARM_MODE=$ARM_MODE -DCMAKE_MAKE_PROGRAM=$ANDROID_NDK_HOME/prebuilt/linux-x86_64/bin/make -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake
